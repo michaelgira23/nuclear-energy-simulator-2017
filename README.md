ebc2d9de71b8552c891e7cfa5e48f819c05ed7dc
Michael Gira <contact@michaelgira.me>
2017-05-12 01:00:46 -0500

    Add reactors to sidebar, enhance README, add pollution

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# nuclear-power-simulator-2017
+# Nuclear Power Simulator 2017
 Plan, build, and manage nuclear power plants to become the greenest country on the planet
+
+## The Goal
+You're the president of $country and you're on a mission: embrace the power of nuclear energy! Throughout the way, you will encounter many of the political, social, and economic challenges that come with the adoption of nuclear energy. Convert your country to 100% nuclear to win!
--- src/game/game.ts
+++ src/game/game.ts
@@ -16,6 +16,7 @@ export class Game {
 
 	private _money: number;
 	private _moneyGained: number;
+	private _pollution: number;
 	private _time: number;
 
 	// El DINERO
@@ -46,6 +47,14 @@ export class Game {
 		this.$game.find('.status-moneygained span').text(`${sign}$${value}`);
 	}
 
+	// Percentage between 0 and 1 (inclusive) how much pollution there is
+	get pollution() {
+		return this._pollution;
+	}
+	set pollution(value) {
+		this._pollution = value;
+	}
+
 	// How many game ticks have passed
 	get time() {
 		return this._time;
@@ -58,14 +67,18 @@ export class Game {
 	}
 
 	constructor(query: string, firstName: string, lastName: string) {
+
+		// Game initialization
+
 		this.$game = $(query);
 		this.name = {
 			first: firstName,
 			last: lastName
 		};
 
-		this.money = 420;
+		this.money = 400;
 		this.moneyGained = 100;
+		this.pollution = 0.8
 
 		this.time = 0;
 		setInterval(() => {
@@ -73,5 +86,19 @@ export class Game {
 				this.money += this.moneyGained;
 			}
 		}, this.gameTick);
+
+		// Add reactors from array into the HTML
+		const $buy = this.$game.find('.buy-reactors');
+		for (const size of Object.keys(reactorSpecs)) {
+			const reactor = reactorSpecs[size];
+			$buy.append(`
+				<div class="buy-reactor">
+					<h5 class="reactor-name">${size[0].toUpperCase() + size.substr(1)} Reactor</h5>
+					<img class="reactor-image" src="images/reactors/${size}/${size}.png">
+					<p>Cost <strong>${reactor.cost}</strong></p>
+					<p>Generates <strong>${reactor.mw} MW</strong></p>
+				</div>
+			`);
+		}
 	}
 }
